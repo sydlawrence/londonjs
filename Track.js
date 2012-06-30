d77544f8dc934505b0a7d277744798d438119d45
Syd Lawrence <sydlawrence@gmail.com>
2012-06-30 20:59:15 +0100

    made it use live, not stage toma.hk

--- Track.js
+++ Track.js
@@ -5,7 +5,7 @@ var Track = function(artist, title) {
 	this.render = function() {
 
 		var iframe = $("<iframe src='' width=200 height=200/>");
-		iframe.attr("src", "http://stage.toma.hk/embed.php?artist="+artist+"&title="+title);
+		iframe.attr("src", "http://toma.hk/embed.php?artist="+artist+"&title="+title);
 
 		$('#results').append(iframe);
 
